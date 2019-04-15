6066178720323e55cd7cb225a2b3c0a289d5a803
MathiasWickenhagen <31931798+MathiasWickenhagen@users.noreply.github.com>
2019-04-15 14:27:45 +0200

    Add youtube.js

---
+++ src/electron/apis/youtube.js
@@ -0,0 +1,53 @@
+const _ = require('../../js/utils.js');
+const YouTube = require('simple-youtube-api');
+let youtube;
+
+module.exports = apikey => {
+
+    // Create connection
+    youtube = new YouTube(apikey);
+
+    return {
+        getPlaylistItems: getPlaylistItems
+    };
+};
+
+/**
+ * Get all items of a playlist
+ *
+ * @param listId
+ * @param props
+ * @returns {Promise<PromiseLike<T | never>>}
+ */
+async function getPlaylistItems(listId, ...props) {
+
+    // Start request
+    return youtube.getPlaylistByID(listId)
+
+    // Get the videos of the playlist
+        .then(playlist => playlist.getVideos())
+
+        // Filter response
+        .then(videos => {
+
+            // Exits a filter
+            if (props.length > 0) {
+
+                // Props video list
+                const playListItems = [];
+
+                // Get each video
+                for (const video of videos) {
+
+                    // Filter video info and add to the list
+                    playListItems.push(_.pick(video, props));
+                }
+
+                // Return the props video list
+                return playListItems;
+            }
+
+            // Return the raw video list
+            return videos;
+        });
+}
--- src/js/utils.js
+++ src/js/utils.js
@@ -153,3 +153,19 @@ module.exports.throttleEvent = (fn, ms = 1000) => {
         timeout = setTimeout(() => fn(...args), ms);
     };
 };
+
+/**
+ * Pick specific props from an object
+ * @param object
+ * @param props
+ */
+module.exports.pick = (object, props) => {
+    const newObj = {};
+
+    for (let i = 0, l = props.length; i < l; i++) {
+        const prop = props[i];
+        newObj[prop] = object[prop];
+    }
+
+    return newObj;
+}
